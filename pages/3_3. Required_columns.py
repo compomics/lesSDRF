cec2ab6c269afa610a7189ff4cd1302edccb176c
TineClaeys <tine.claeys2@hotmail.com>
2023-05-08 16:19:48 +0200

    cell line fix

--- pages/3_3. Required_columns.py
+++ pages/3_3. Required_columns.py
@@ -210,9 +210,9 @@ if selection == "characteristics[cell type]":
 
 if selection == "characteristics[cell line]":
     st.subheader("Input the cell line of your sample if one was used")
-    all_celltype = data_dict["all_cell_type_elements"]
-    celltype_nodes = data_dict["cell_type_nodes"]
-    df = ParsingModule.multiple_ontology_tree(selection, all_celltype, celltype_nodes, template_df,multiple_in_one=True)
+    all_cellline = data_dict["all_cell_line_elements"]
+    cellline_nodes = data_dict["cell_line_nodes"]
+    df = ParsingModule.multiple_ontology_tree(selection, all_cellline, cellline_nodes, template_df,multiple_in_one=True)
     update_session_state(df)
 
 if selection == "comment[cleavage agent details]":
